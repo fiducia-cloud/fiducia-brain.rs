3f1d4ec1f298df26d57c5f498dc6208be1166b9f
alex-mills <alexander.d.mills@gmail.com>
2026-07-22 21:05:56 -0500

    Upgrade telemetry lifecycle to v0.2.1

--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,7 @@ const MAX_BODY_BYTES: usize = 256 * 1024;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    fiducia_telemetry::init(SERVICE);
+    let _telemetry = fiducia_telemetry::init(SERVICE);
 
     // Authoritative cluster configuration: shard_count (fixed) + replication
     // factor. Everything else reads this.
